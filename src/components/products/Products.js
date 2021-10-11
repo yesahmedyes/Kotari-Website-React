9eb281a6235c43bd10a9e024b6afd5d1ff8218f9
Ahmed Haroon <ahmed.dys99@gmail.com>
2021-10-10 23:06:07 -0400

    check

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
 import Product from './product/Product'
-import useStyles from "./styles";
 const Products=({products,addToCart})=>{
-  const classes=useStyles();
   return(
     <div class="container mydiv">
     <div class="row">
